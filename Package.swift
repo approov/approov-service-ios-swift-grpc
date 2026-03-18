e769db05cb5532bd8c99b17a36d5821849940619
ivo liondov <ivoliondov@Sanctuary.local>
2026-03-18 11:06:57 +0000

    Make approov sdk a package dependency conditionally to support multy service layers usage

--- Package.swift
+++ Package.swift
@@ -1,14 +1,14 @@
 // swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
+import PackageDescription
 // Release tag
 let releaseTAG = "3.5.3"
 // SDK package version (used for both iOS and watchOS)
-let sdkVersion = "3.5.3"
+let sdkVersion: Version = "3.5.3"
 
 
 let package = Package(
-    name: "ApproovSession",
+    name: "ApproovGRPCSession",
     platforms: [
         .iOS(.v11),
         .watchOS(.v9)
@@ -16,33 +16,29 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "ApproovSession",
-            targets: ["ApproovSession"]
+            name: "ApproovGRPCSession",
+            targets: ["ApproovGRPCSession"]
         ),
-        .library(name: "ApproovSessionDynamic", type: .dynamic, targets: ["ApproovSession"])
+        .library(name: "ApproovGRPCSessionDynamic", type: .dynamic, targets: ["ApproovGRPCSession"])
     ],
     dependencies: [
         // Package's external dependencies and from where they can be fetched:
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0")),
-        .package(url: "https://github.com/apple/swift-http-structured-headers.git", from: "1.0.0")
+        .package(url: "https://github.com/approov/approov-ios-sdk.git", exact: sdkVersion),
+        .package(url: "https://github.com/grpc/grpc-swift.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "ApproovSession",
+            name: "ApproovGRPCSession",
             dependencies: [
-                "Approov",
-                "Alamofire",
-                .product(name: "RawStructuredFieldValues", package: "swift-http-structured-headers")
+                .product(name: "Approov", package: "approov-ios-sdk"),
+                .product(name: "GRPC", package: "grpc-swift"),
+                .product(name: "Logging", package: "swift-log")
             ],
-            path: "Sources/ApproovSession",  // Point to the shared source code
+            path: "Sources/ApproovGRPC",
             exclude: ["README.md", "LICENSE"]
-        ),
-        // Binary target for the merged xcframework
-        .binaryTarget(
-            name: "Approov",
-            url: "https://github.com/approov/approov-ios-sdk/releases/download/\(sdkVersion)/Approov.xcframework.zip"
         )
     ]
 )
